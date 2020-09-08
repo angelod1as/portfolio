b7d403e0502b379b0db8c2ba876908b508862951
Angelo Dias <oiangelodias@gmail.com>
2020-09-08 18:10:30 -0300

    Page splitting

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,87 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import parse from 'html-react-parser';
-import styled from 'styled-components';
 
-import parserOptions from '../components/parser';
-import Container from '../components/container';
-
-import Sidebar from '../components/Sidebar';
-import Mosaic from '../fragments/Mosaic';
-import Html from '../components/html-styles';
-import size from '../components/breakpoints';
 import colors from '../components/colors';
 
-const width = '20%';
+import NotFoundPage from './pages/NotFound';
+import SinglePage from './pages/SinglePage';
+import MosaicPage from './pages/MosaicPage';
 
-const Grid = styled.div`
-  color: ${props => props.theme.color.black};
-  background-color: ${props => props.theme.color.white};
-  font-family: ${props => props.theme.font.display};
-  height: 100%;
-
-  display: grid;
-  grid-template-areas: 'sidebar content';
-  grid-template-columns: ${width} auto;
-  position: relative;
-
-  @media ${size.medium} {
-    display: block;
-    height: 100%;
-  }
-`;
-
-const SidebarHolder = styled.div`
-  grid-area: sidebar;
-
-  padding: 40px;
-  position: fixed;
-  width: 20%;
-  height: 100%;
-
-  background-color: ${p => p.color};
-  color: ${p => p.theme.color.white};
-
-  @media ${size.medium} {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: auto;
-  }
-`;
-
-const Content = styled.div`
-  grid-area: content;
-
-  padding: 40px;
-  background-color: ${p => p.theme.color.white};
-  color: ${p => p.theme.color.black};
-
-  @media ${size.medium} {
-    display: block;
-    width: 100%;
-  }
-`;
-
-const Page = props => {
-  const {
-    children,
-    notFound,
-    pageContext,
-    pageContext: { type: contextType },
-    location,
-    data: {
-      pageInfo: {
-        frontmatter: { title, live },
-        excerpt,
-        html,
-      },
-      items,
+const Page = ({
+  children,
+  notFound,
+  pageContext: { type: contextType },
+  location,
+  data: {
+    pageInfo: {
+      frontmatter: { title, live },
+      excerpt,
+      html,
     },
-    path,
-  } = props;
-
+    items,
+  },
+  path,
+}) => {
   const { state } = location;
   let from = null;
   if (state) {
@@ -92,100 +33,57 @@ const Page = props => {
 
   const color = colors[Math.floor(Math.random() * colors.length)];
 
-  if (contextType === 'notFound') {
+  if (notFound || contextType === 'notFound') {
     return (
-      <Container seo={seo}>
-        <Grid>
-          <SidebarHolder color={color}>
-            <Sidebar
-              excerpt={excerpt}
-              live={live}
-              path={path}
-              type={contextType}
-              title={title}
-              color={color}
-              from={from}
-            />
-          </SidebarHolder>
-          <Content>
-            <h2>Page not found</h2>
-            <p>Click on the "back" link to go... well, back.</p>
-          </Content>
-        </Grid>
-      </Container>
+      <NotFoundPage
+        info={{
+          seo,
+          color,
+          excerpt,
+          live,
+          path,
+          contextType,
+          title,
+          from,
+        }}
+      />
     );
   }
 
   if (contextType === 'singlepage') {
     return (
-      <Container seo={seo}>
-        <Grid>
-          <SidebarHolder color={color}>
-            <Sidebar
-              excerpt={excerpt}
-              live={live}
-              path={path}
-              type={contextType}
-              title={title}
-              color={color}
-              from={from}
-              singlePage
-            />
-          </SidebarHolder>
-          <Content>{children}</Content>
-        </Grid>
-      </Container>
+      <SinglePage
+        info={{
+          seo,
+          color,
+          excerpt,
+          live,
+          path,
+          contextType,
+          title,
+          from,
+        }}
+      >
+        {children}
+      </SinglePage>
     );
   }
 
-  const collection = items.edges.map(({ node }) => {
-    if (contextType === 'projects') {
-      const { frontmatter } = node;
-      const { fullPath } = node.fields;
-      const image = items.edges.filter(each => {
-        const imageId = each.node.frontmatter.image.childImageSharp.id;
-        return frontmatter.image.childImageSharp.id === imageId;
-      });
-      frontmatter.image.childImageSharp = image[0].node.frontmatter.image.childImageSharp;
-      return {
-        frontmatter,
-        fullPath,
-      };
-    }
-    return null;
-  });
-
-  const getContent = pageType => {
-    if (pageType === 'projects') return <Mosaic items={collection} color={color} path={path} />;
-    return <Html color={color}>{parse(html, parserOptions())}</Html>;
-  };
-
   return (
-    <Container seo={seo}>
-      <Grid>
-        <SidebarHolder color={color}>
-          <Sidebar
-            excerpt={excerpt}
-            live={live}
-            path={path}
-            type={contextType}
-            title={title}
-            color={color}
-            from={from}
-          />
-        </SidebarHolder>
-        <Content>
-          {notFound ? (
-            <>
-              <h2>Page not found</h2>
-              <p>Click on the "back" link to go... well, back.</p>
-            </>
-          ) : (
-            getContent(contextType)
-          )}
-        </Content>
-      </Grid>
-    </Container>
+    <MosaicPage
+      info={{
+        seo,
+        color,
+        excerpt,
+        live,
+        path,
+        contextType,
+        title,
+        from,
+        html,
+        items,
+      }}
+    />
   );
 };
 
