670b48975d1f4d2dac4d422de13b3cc8f959b54b
Angelo Dias <oiangelodias@gmail.com>
2020-11-23 20:01:23 -0300

    Add Styled Components

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Teste</h1>
+      </main>
+
+      <footer>
+        <p>Teste</p>
+      </footer>
+    </div>
+  )
+}
