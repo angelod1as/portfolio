29a3906e9c69f90d4884d1a3126c945e737a97d1
Angelo Dias <oiangelodias@gmail.com>
2020-11-23 19:23:32 -0300

    Add lint + Typescript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,16 +24,17 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'  // Make sure this is always the last element in the array.
+    'plugin:prettier/recommended' // Make sure this is always the last element in the array.
   ],
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'simple-import-sort/sort': 'error',
+    'simple-import-sort/imports': 'error',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
