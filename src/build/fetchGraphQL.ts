50866e4c3adf9f41d947251b5b8772777be337b2
Angelo Dias <oiangelodias@gmail.com>
2020-11-26 21:36:09 -0300

    Add types

--- src/build/fetchGraphQL.ts
+++ src/build/fetchGraphQL.ts
@@ -2,7 +2,6 @@ const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
 const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
 
 export async function fetchContent<T>(query: string): Promise<T> {
-  console.log('\n\nFetching data')
   try {
     const res = await fetch(`https://graphql.contentful.com/content/v1/spaces/${space}`, {
       method: 'POST',
@@ -13,7 +12,6 @@ export async function fetchContent<T>(query: string): Promise<T> {
       body: JSON.stringify({ query }),
     })
     const { data } = await res.json()
-    console.log('\n\nData fetched')
     return data
   } catch (error) {
     console.error(`There was a problem retrieving entries with the query ${query}`)
