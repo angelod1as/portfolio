fe4544d517645712141f3c106d27aded82132fe8
Angelo Dias <oiangelodias@gmail.com>
2021-05-30 13:38:52 -0300

    Remove styles from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/!(*.stories).{ts,tsx}'],
+  collectCoverageFrom: ['src/**/!(*.stories).{ts,tsx}', '!**/styles.ts'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   // moduleDirectories: ['node_modules', 'src'],
