bac971339673689a7d7093d9b0dc4b16b4e47fe0
Angelo Dias <oiangelodias@gmail.com>
2021-12-26 19:08:59 -0300

    Add home, contact & footer

---
+++ src/components/common/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function Contact() {
+  return (
+    <div>
+      <h2>contact me anytime</h2>
+
+      <a href="https://twitter.com/oicronofobico">twitter</a>
+      <a href="https://instagram.com/oicronofobico">instagram</a>
+      <a href="https://t.me/oicronofobico">telegram</a>
+      <a href="mailto:oiangelodias@gmail.com">email</a>
+    </div>
+  )
+}
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,13 +1,41 @@
+import { Footer } from '#components/common/Footer'
 import React from 'react'
 
 export function Home() {
   return (
-    <div>
-      <h1>
-        I am angelo and I do <i>stuff</i>
-      </h1>
+    <>
+      <div>
+        <h1>
+          I'm angelo and I do <i>stuff</i>
+        </h1>
 
-      <p>I'm a developer, writer and designer</p>
-    </div>
+        <div>
+          <p>I'm a developer, writer and designer.</p>
+          <p>I'm a Scrum Master and awarded journalist.</p>
+          <p>I make music and I read tarot.</p>
+          <p>I have a daily podcast with short episodes.</p>
+        </div>
+
+        <h2>I'm a proud generalist</h2>
+
+        <h3>highlighted projects</h3>
+
+        {/* Highlighted */}
+
+        <h2>you are free to explore new stuff</h2>
+
+        {/* Mosaic? */}
+
+        {/* Extra  */}
+
+        <h2>you can always rely on a blog</h2>
+
+        <h3>latest posts</h3>
+
+        {/* Latest posts */}
+
+        <Footer />
+      </div>
+    </>
   )
 }
