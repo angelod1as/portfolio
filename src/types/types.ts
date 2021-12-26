50eb842e507848abb808d4ed82bf4e901c84c9d6
Angelo Dias <oiangelodias@gmail.com>
2021-12-26 13:41:21 -0300

    Add mdx and types

---
+++ src/types/types.ts
@@ -0,0 +1,41 @@
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
+
+// API
+// export type Credits = {
+//   name: string;
+//   roles: string;
+//   gap?: boolean;
+//   url?: string;
+// };
+
+// export type Images = {
+//   path?: string;
+//   width?: number;
+//   height?: number;
+//   alt: string;
+//   video?: string;
+// };
+
+// export type FrontMatter = {
+//   title: string;
+//   excerpt: string;
+//   highlight: boolean;
+//   extraInfo: string;
+//   images: Images[];
+//   credits: Credits[];
+//   order: number;
+// };
+
+// export type FileData = {
+//   data: FrontMatter;
+//   content: string;
+// };
+
+// export type MdxProject = MDXRemoteSerializeResult<FrontMatter>;
+
+// export type About = {
+//   about: MDXRemoteSerializeResult<FrontMatter>;
+//   contact: MDXRemoteSerializeResult<FrontMatter>;
+// };
+
+// export type Locale = "pt-BR" | "en-US";
