4dbe57cb823502eecfa750ea42089eaab868616c
Angelo Dias <oiangelodias@gmail.com>
2021-12-27 19:48:52 -0300

    Increase title sizes

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,37 +1,30 @@
-import { Footer } from '#components/common/Footer'
 import { Highlighted } from '#components/pages/Home/Highlighted/Highlighted'
 import { LatestPosts } from '#components/pages/Home/LatestPosts'
 import React from 'react'
 import { Mosaic } from './Mosaic'
 import { Summary } from './Summary/Summary'
 
+// Add :stars: to Stuff
+
 export function Home() {
   return (
     <>
       <div className="flex flex-col gap-16 px-4 pt-32 pb-16">
-        <h1>
-          I'm angelo and I do <i>stuff</i>
-        </h1>
+        <h1>I'm angelo and I do stuff</h1>
 
         <Summary />
 
-        <h2>I'm a proud generalist</h2>
-
         <Highlighted />
 
-        <h2>you are free to explore new stuff</h2>
+        <h2 className="text-7xl">I'm a proud generalist</h2>
 
         <Mosaic />
 
         {/* Extra  */}
 
-        <h2>you can always rely on a blog</h2>
+        <h2 className="text-7xl">you can always rely on a blog</h2>
 
         <LatestPosts />
-
-        {/* Latest posts */}
-
-        <Footer />
       </div>
     </>
   )
