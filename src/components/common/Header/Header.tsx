a2d39ffe39c538d0cbe2e4ec35f356a367a81650
Angelo Dias <oiangelodias@gmail.com>
2021-12-27 19:54:48 -0300

    Add z-index to Header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Header: FC<Props> = ({ isHome }) => {
   return (
     <div
-      className={`fixed top-0 left-0 flex justify-between w-full px-4 py-2 bg-black ${style.shadow}`}
+      className={`fixed top-0 left-0 flex justify-between w-full px-4 py-2 bg-black z-50 ${style.shadow}`}
     >
       <div className="flex gap-4">
         <InnerLink href="blog">blog</InnerLink>
