cf5b9e7b8ecd2576d9f98a0be58b04dcc4c6a6da
Angelo Dias <oiangelodias@gmail.com>
2022-01-02 10:27:21 -0300

    Add MDX Provider

--- src/components/pages/Providers/Providers.tsx
+++ src/components/pages/Providers/Providers.tsx
@@ -1,15 +1,20 @@
 import { Footer } from '#components/common/Footer'
 import { Header } from '#components/common/Header'
+import { MDXProvider } from '@mdx-js/react'
 import React, { FC } from 'react'
+
+const components = {}
 
 export const Providers: FC = ({ children }) => {
   return (
-    <div className="relative min-h-screen text-white bg-black ">
-      <Header isHome={false} />
-      <div className="flex flex-col max-w-xl gap-16 px-4 pt-32 pb-16 m-auto">
-        {children}
+    <MDXProvider components={components}>
+      <div className="relative min-h-screen text-white bg-black ">
+        <Header isHome={false} />
+        <div className="flex flex-col max-w-xl gap-16 px-4 pt-32 pb-16 m-auto">
+          {children}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </MDXProvider>
   )
 }
