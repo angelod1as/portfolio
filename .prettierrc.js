b9bf82e4f53efd9a728b3314407f18089f59fff2
Angelo Dias <oiangelodias@gmail.com>
2022-01-23 11:03:38 -0300

    Removed deprecated property

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,8 @@
 module.exports = {
-  "semi": false,
-  "tabWidth": 2,
-  "printWidth": 80,
-  "singleQuote": true,
-  "trailingComma": "es5",
-  "jsxBracketSameLine": false,
-  "arrowParens": "avoid"
+  semi: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
 }
