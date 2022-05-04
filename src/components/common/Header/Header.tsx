84f80bdffcc7f4564a89dc8b40170210504dbdc8
Angelo Dias <oiangelodias@gmail.com>
2022-05-04 23:36:32 +0200

    Remove header from home

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import React, { FC } from 'react'
 import { RandomColors } from 'src/helpers/colors'
 import style from './Header.module.sass'
@@ -8,6 +9,8 @@ type HeaderProps = {
 }
 
 export const Header: FC<HeaderProps> = ({ colors }) => {
+  const { asPath } = useRouter()
+
   return (
     <div
       className={`fixed top-0 left-0 flex justify-between w-full px-4 py-2 bg-black z-50 ${style.shadow}`}
@@ -17,11 +20,13 @@ export const Header: FC<HeaderProps> = ({ colors }) => {
         <MenuLink content="blog" href="/blog" />
         <MenuLink content="podcast" href="/cronofobia" />
       </div>
-      <div className="hidden md:block">
-        <b>
-          I'm <span className={colors.textColor}>angelo</span> and I do stuff
-        </b>
-      </div>
+      {asPath !== '/' && (
+        <div className="hidden md:block">
+          <b>
+            I'm <span className={colors.textColor}>angelo</span> and I do stuff
+          </b>
+        </div>
+      )}
     </div>
   )
 }
