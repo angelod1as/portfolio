081517a494ab95c6e4199115d460affd99efe065
Angelo Dias <oiangelodias@gmail.com>
2022-05-09 14:54:09 +0200

    Remove loop from Parenthesis

--- src/components/common/MDX/globalComponents.tsx
+++ src/components/common/MDX/globalComponents.tsx
@@ -1,16 +1,18 @@
 import { FCC } from '#types/types'
 import { CTAProps } from '../CTA'
-import { ParenthesisProps } from './Parenthesis'
+import { Parenthesis, ParenthesisProps } from './Parenthesis'
 
 // Strikethrough
 const S: FCC = props => <s {...props} />
 const CTA: FCC<CTAProps> = props => <CTA {...props} />
-const Parenthesis: FCC<ParenthesisProps> = props => <Parenthesis {...props} />
+const ParenthesisComponent: FCC<ParenthesisProps> = props => (
+  <Parenthesis {...props} />
+)
 
 export const globalComponents = () => {
   return {
     CTA,
     S,
-    Parenthesis,
+    Parenthesis: ParenthesisComponent,
   }
 }
