92c589fde01c67a89f4fe78a1d1ac3c08bc6d6c6
Angelo Dias <oiangelodias@gmail.com>
2022-05-24 19:56:07 +0200

    Add rss feed

---
+++ src/lib/RSS/generateRssFeed.ts
@@ -0,0 +1,58 @@
+import { BlogPostMetadata } from '#types/types'
+import { Feed } from 'feed'
+import fs from 'fs'
+
+type Posts = Array<{
+  metadata: Partial<BlogPostMetadata> | undefined
+  slug: string
+}>
+
+export const generateRssFeed = async (posts: Posts) => {
+  const baseURL = 'https://www.angelodias.com.br'
+  const author = {
+    name: 'Angelo Dias',
+    email: 'oiangelodias@gmail.com',
+    link: 'https://www.angelodias.com.br',
+  }
+  const now = new Date()
+
+  const feed = new Feed({
+    title: 'My name is Angelo and I do Blogging',
+    description: 'Thoughts and ideas on anything, from tech to nonsense',
+    id: `${baseURL}/blog`,
+    link: `${baseURL}/blog`,
+    updated: now,
+    language: 'en',
+    image: `${baseURL}/social.png`,
+    favicon: `${baseURL}/favicon.ico`,
+    copyright: 'All rights reserved 2021, Angelo Dias',
+    generator: 'awesome', // optional, default = 'Feed for Node.js'
+    feedLinks: {
+      json: `${baseURL}/json`,
+      atom: `${baseURL}/atom`,
+    },
+    author,
+  })
+
+  posts.forEach(({ metadata, slug }) => {
+    const url = `${baseURL}/blog/${slug}`
+    if (metadata && !metadata.draft) {
+      const { createdAt, description, title } = metadata
+      feed.addItem({
+        title: title ?? 'Missing title!',
+        id: url,
+        link: url,
+        description: description,
+        content: description,
+        author: [author],
+        contributor: [author],
+        date: createdAt ? new Date(createdAt) : now,
+      })
+    }
+  })
+
+  fs.mkdirSync('./public/rss', { recursive: true })
+  fs.writeFileSync('./public/rss/feed.xml', feed.rss2())
+  fs.writeFileSync('./public/rss/atom.xml', feed.atom1())
+  fs.writeFileSync('./public/rss/feed.json', feed.json1())
+}
