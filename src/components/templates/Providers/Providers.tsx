cbd3b24205abb25b6b7ad15dbd74027140b35d07
Angelo Dias <oiangelodias@gmail.com>
2022-05-26 18:32:47 +0200

    Update providers

--- src/components/templates/Providers/Providers.tsx
+++ src/components/templates/Providers/Providers.tsx
@@ -1,10 +1,8 @@
-import { Footer } from '#components/common/Footer'
-import { Header } from '#components/common/Header'
-import ProgressBar from '#components/common/ProgressBar/ProgressBar'
 import { FCC } from '#types/types'
 import { MDXProvider } from '@mdx-js/react'
 import React from 'react'
 import { RandomColors } from 'src/helpers/colors'
+import { Template } from '../Wrappers/Template'
 import { ColorContextProvider } from './ColorProvider'
 
 const components = {}
@@ -18,14 +16,7 @@ export const Providers: FCC<ProvidersType> = ({ children, colors, slug }) => {
   return (
     <ColorContextProvider value={{ colors }}>
       <MDXProvider components={components}>
-        <div className="relative min-h-screen text-white bg-black ">
-          <Header />
-          <div className="flex flex-col max-w-xl gap-16 px-4 pt-32 pb-16 m-auto md:pt-40">
-            {children}
-          </div>
-          <Footer slug={slug} />
-          <ProgressBar />
-        </div>
+        <Template slug={slug}>{children}</Template>
       </MDXProvider>
     </ColorContextProvider>
   )
