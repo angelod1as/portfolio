e8dcfa6259470de2a68a09c4acb750511691cc87
Angelo Dias <oiangelodias@gmail.com>
2022-05-27 09:18:28 +0200

    Close subscriptions

--- src/components/common/Subscribe/Subscribe.tsx
+++ src/components/common/Subscribe/Subscribe.tsx
@@ -1,10 +1,7 @@
 import { useColorContext } from '#components/templates/Providers/ColorProvider'
 import React from 'react'
-import {
-  textColor as defaultTextColor,
-  bgColor as defaultBgColor,
-  borderColor as defaultBorderColor,
-} from 'src/helpers/colors'
+import { Link } from '../Links'
+import { Strong } from '../Strong'
 
 type SubscribeProps = {
   inner?: boolean
@@ -12,45 +9,64 @@ type SubscribeProps = {
 
 export const Subscribe = ({ inner }: SubscribeProps) => {
   const { colors } = useColorContext()
-  const textColor = colors?.textColor ?? defaultTextColor[0]
-  const bgColor = colors?.bgColor ?? defaultBgColor[0]
-  const borderColor = colors?.borderColor ?? defaultBorderColor[0]
 
   return (
-    <form method="post" action="https://blogtrottr.com">
-      <h2 className={`mb-10 ${inner ? '' : 'h2-as-h1'}`}>
-        <span className={textColor}>Subscribe</span> to my blog
+    <div>
+      <h2 className={`mb-10`}>
+        <span className={colors.textColor}>Subscribe</span> to my blog
+        <p>
+          Oops, subscriptions are{' '}
+          <Strong color={colors.textColor}>closed</Strong> for now.{' '}
+          <Link inner href="blog/2022-05-27-about-subscription">
+            read here to know more.
+          </Link>
+        </p>
       </h2>
-      <div className="flex items-center gap-4 mb-8">
-        <label htmlFor="btr_email" className="whitespace-nowrap">
-          Your email:
-        </label>
-        <input
-          type="text"
-          name="btr_email"
-          id="btr_email"
-          placeholder="mrspock@enterprise.com"
-          className="w-full"
-        />
-
-        <input
-          type="hidden"
-          name="btr_url"
-          value="https://www.angelodias.com.br/rss/feed.xml"
-        />
-        <input type="hidden" name="schedule_type" value="0" />
-        <input
-          type="submit"
-          value="Follow"
-          className={`${bgColor} ${borderColor}`}
-        />
-      </div>
-      <p>
-        PS: I use a free service called{' '}
-        <a href="https://blogtrottr.com/">blogtrottr</a> to manage blog-to-email
-        posts instead of a full-blown newsletter service. It's free (for you and
-        for me) and very configurable.
-      </p>
-    </form>
+    </div>
   )
 }
+
+// export const Subscribe = ({ inner }: SubscribeProps) => {
+// const { colors } = useColorContext()
+//   const textColor = colors?.textColor ?? defaultTextColor[0]
+//   const bgColor = colors?.bgColor ?? defaultBgColor[0]
+//   const borderColor = colors?.borderColor ?? defaultBorderColor[0]
+
+//   return (
+//     <form method="post" action="https://blogtrottr.com">
+// <h2 className={`mb-10 ${inner ? '' : 'h2-as-h1'}`}>
+//   <span className={textColor}>Subscribe</span> to my blog
+// </h2>
+//       <div className="flex items-center gap-4 mb-8">
+//         <label htmlFor="btr_email" className="whitespace-nowrap">
+//           Your email:
+//         </label>
+//         <input
+//           type="text"
+//           name="btr_email"
+//           id="btr_email"
+//           placeholder="mrspock@enterprise.com"
+//           className="w-full"
+//         />
+
+//         <input
+//           type="hidden"
+//           name="btr_url"
+//           value="https://www.angelodias.com.br/rss/feed.xml"
+//         />
+//         <input type="hidden" name="schedule_type" value="0" />
+//         <input
+//           type="submit"
+//           value="Follow"
+//           className={`${bgColor} ${borderColor}`}
+//         />
+//       </div>
+//       <p>
+//         PS: I use a free service called{' '}
+//         <a href="https://blogtrottr.com/">blogtrottr</a> to manage blog-to-email
+//         posts instead of a full-blown newsletter service. It's free (for you and
+//         for me) and very configurable.
+//       </p>
+//     </form>
+//   )
+// }
