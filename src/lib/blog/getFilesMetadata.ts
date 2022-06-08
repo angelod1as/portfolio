9cdb9be395a7151cddbf6351115a20213a48d02a
Angelo Dias <oiangelodias@gmail.com>
2022-06-08 14:22:45 +0200

    Fix fetchAllPosts

--- src/lib/blog/getFilesMetadata.ts
+++ src/lib/blog/getFilesMetadata.ts
@@ -2,16 +2,15 @@ import { serialize } from '#lib/MDX/serialize'
 import { BlogPagePostMetadata } from '#pages/blog'
 import { readFileSync } from 'fs'
 import matter from 'gray-matter'
-import { join } from 'path'
 import { splitDirAndFiles } from './splitDirAndFiles'
 
-export const getFilesMetadata = async (fileNames: string[]) => {
-  const postsMetadatas = fileNames.map(async fileName => {
-    const [directory, filename] = splitDirAndFiles(fileName)
-    const filePath = join(directory, filename)
+export const getFilesMetadata = async (filePaths: string[]) => {
+  const postsMetadatas = filePaths.map(async filePath => {
+    const [, filename] = splitDirAndFiles(filePath)
     const fileContent = readFileSync(filePath, 'utf-8')
     const { data } = matter(fileContent)
-    const slug = fileName.replace(/\.mdx?/, '')
+    const slug = filename.replace(/\.mdx?/, '')
+
     const compiledTitle = (await serialize({ content: data.title }))
       .compiledSource
 
--- src/lib/blog/splitDirAndFiles.ts
+++ src/lib/blog/splitDirAndFiles.ts
@@ -2,6 +2,6 @@ export const splitDirAndFiles = (file: string) => {
   const split = file.split('/')
   const length = split.length - 1
   const filename = split[length]
-  const contentDir = split.splice(0, length).join('/')
-  return [contentDir, filename]
+  const directory = split.splice(0, length).join('/')
+  return [directory, filename]
 }
