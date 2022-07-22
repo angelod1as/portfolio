d7487a1dadad3b921fc49dbb65cbf5647bc8ed09
Angelo Dias <oiangelodias@gmail.com>
2022-07-22 21:34:06 +0200

    Add Substack

--- src/components/common/Subscribe/Subscribe.tsx
+++ src/components/common/Subscribe/Subscribe.tsx
@@ -1,7 +1,5 @@
 import { Closed } from './components/closed'
-import { useColorContext } from '#components/templates/Providers/ColorProvider'
-import { textColor as defaultTextColor } from 'src/helpers/colors'
-import { Substack } from './components/customSubstackWidget'
+import { Substack } from './components/substack'
 
 type SubscribeProps = {
   inner?: boolean
@@ -9,26 +7,13 @@ type SubscribeProps = {
 }
 
 export const Subscribe = ({ inner, closed }: SubscribeProps) => {
-  const { colors } = useColorContext()
-  const textColor = colors?.textColor ?? defaultTextColor[0]
-
   if (closed) {
     return <Closed />
   }
 
   return (
     <div>
-      <h2 className={`${inner ? '' : 'mb-4 h2-as-h1'}`}>
-        <span className={textColor}>Subscribe</span> to my blog
-      </h2>
-
-      <p className={`${inner ? 'mb-4' : 'mb-4'}`}>
-        <small>
-          Very ocasional, 100% not spammy, just general updates and new articles
-        </small>
-      </p>
-
-      <Substack />
+      <Substack inner={inner} />
     </div>
   )
 }
