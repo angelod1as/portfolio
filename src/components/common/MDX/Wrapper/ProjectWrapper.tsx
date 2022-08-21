87df573c79435427dd8bc6a84bbc1551c58d097b
Angelo Dias <oiangelodias@gmail.com>
2022-08-21 19:00:23 +0200

    Improve CSS

--- src/components/common/MDX/Wrapper/ProjectWrapper.tsx
+++ src/components/common/MDX/Wrapper/ProjectWrapper.tsx
@@ -1,6 +1,5 @@
 import { FCC, Metadata } from '#types/types'
 import React from 'react'
-import { TimestampToDate } from 'src/helpers/TimestampToDate'
 
 type ProjectWrapperProps = {
   metadata: Metadata
@@ -12,30 +11,5 @@ export const ProjectWrapper: FCC<ProjectWrapperProps> = ({
 }) => {
   const { createdAt, timeToRead, wordCount, tags } = metadata
 
-  return (
-    <>
-      <p className="flex flex-wrap gap-4 mb-8 text-xs text-gray-400">
-        {!!createdAt && <span>Published at {TimestampToDate(createdAt)}</span>}
-        <span>±{timeToRead} minute read</span>
-        <span>{wordCount} words</span>
-        {tags?.length && <span>Tags: {tags.join(', ')}</span>}
-      </p>
-
-      {children}
-
-      <div className="mt-8 text-gray-400">
-        <p className="text-sm">Thanks for reading.</p>
-        <p className="text-sm">
-          Even though this blog has no comments section, I'd love to hear your
-          thoughts about this article.
-        </p>
-        <p className="text-sm">
-          Tag me on <a href="https://twitter.com/oicronofobico">Twitter</a>,{' '}
-          <a href="https://instagram.com/oicronofobico">Instagram</a> or{' '}
-          <a href="https://t.me/oicronofobico">Telegram</a> using @oicronofobico
-          and let's get this conversation started
-        </p>
-      </div>
-    </>
-  )
+  return <>{children}</>
 }
