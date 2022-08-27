68410ba8cf64e3a6678ba201beb3e9b33527f3ec
Angelo Dias <oiangelodias@gmail.com>
2022-08-27 19:51:39 +0200

    Add more RSS info

--- src/components/common/Subscribe/components/rss.tsx
+++ src/components/common/Subscribe/components/rss.tsx
@@ -4,13 +4,10 @@ import React from 'react'
 export const Rss = () => {
   return (
     <p className="text-sm text-gray-400">
-      If you prefer to subscribe just to the blog, I recommend getting an RSS
-      reader like <Link href="https://newsblur.com/">NewsBlur</Link> and
-      subscribing to my{' '}
+      This blog has an exclusive{' '}
       <Link href="/rss/feed.xml" inner>
         RSS feed
       </Link>
-      .
     </p>
   )
 }
