880ff0684d670919c51b60db81f0c0715c15f7a0
Angelo Dias <oiangelodias@gmail.com>
2022-09-07 22:34:13 +0200

    Update Error and Loader

--- src/components/pages/Blog/NewsletterFeed/NewsletterFeed.tsx
+++ src/components/pages/Blog/NewsletterFeed/NewsletterFeed.tsx
@@ -1,4 +1,6 @@
 import { ContentItem } from '#components/common/ContentItem'
+import { Link } from '#components/common/Links'
+import { Loader } from '#components/common/Loader'
 import { Strong } from '#components/common/Strong'
 import { useColorContext } from '#components/templates/Providers/ColorProvider'
 import React from 'react'
@@ -9,8 +11,24 @@ export const NewsletterFeed = () => {
   const { colors } = useColorContext()
   const { feed, isLoading, isError } = useRSS()
 
-  if (isLoading) return <div>Loading</div>
-  if (isError || !feed) return <div>Error</div>
+  if (isLoading) return <Loader />
+  if (isError || !feed) {
+    return (
+      <div className="text-red">
+        <p>
+          There should be a nice newsletter feed here, but apparently some weird
+          error happened.
+        </p>
+        <p>
+          Please try again later or{' '}
+          <Link href="https://angelodias.substack.com">
+            go straight to the newsletter homepage
+          </Link>
+        </p>
+      </div>
+    )
+  }
+
   return (
     <ul className="flex flex-col gap-16">
       <h2 className="h2-as-h1">
