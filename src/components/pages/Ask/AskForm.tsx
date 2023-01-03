0b5e64d4a4a413d6c74c181146759b938c741e50
Angelo Dias <oiangelodias@gmail.com>
2023-01-03 21:02:35 +0100

    Add Formik to AskForm

--- src/components/pages/Ask/AskForm.tsx
+++ src/components/pages/Ask/AskForm.tsx
@@ -1,33 +1,18 @@
+import { Form } from '#components/common/Form/Form'
+import { Input } from '#components/common/Form/Input'
 import { Link } from '#components/common/Links'
-import { Loader } from '#components/common/Loader'
 import { Strong as StrongModifier } from '#components/common/Strong'
 import { useColorContext } from '#components/templates/Providers/ColorProvider'
-import { ErrorProps, FCC } from '#types/types'
-import { useState } from 'react'
-import {
-  bgColor as defaultBgColor,
-  borderColor as defaultBorderColor,
-  textColor as defaultTextColor,
-} from 'src/helpers/colors'
+import { FCC, PostResult } from '#types/types'
+import { textColor as defaultTextColor } from 'src/helpers/colors'
+import { object as YupObject, string as YupString } from 'yup'
 
-export type AskFormProps = {
+type FormProps = {
   message: string
   name?: string
 }
 
-type FormElements = HTMLFormControlsCollection & {
-  [Key in keyof AskFormProps]: HTMLInputElement
-} & { antirobot: HTMLInputElement }
-
-type AskFormElements = HTMLFormElement & {
-  readonly elements: FormElements
-}
-
-type substackResult = {
-  errors?: ErrorProps[]
-}
-
-const postToNotion = async (props: AskFormProps): Promise<substackResult> => {
+const postToNotion = async (props: FormProps): Promise<PostResult> => {
   const url = '/api/notion/ask/create'
 
   const result = await fetch(url, {
@@ -38,80 +23,38 @@ const postToNotion = async (props: AskFormProps): Promise<substackResult> => {
   return await result.json()
 }
 
-const initialFormValues = {
+const initialValues = {
   name: '',
   message: '',
 }
 
+const validationSchema = YupObject().shape({
+  name: YupString(),
+  message: YupString().required('Ops, parece que você não escreveu nada'),
+})
+
 export const AskForm = () => {
   const { colors } = useColorContext()
-  const bgColor = colors?.bgColor ?? defaultBgColor[0]
-  const borderColor = colors?.borderColor ?? defaultBorderColor[0]
   const textColor = colors?.textColor ?? defaultTextColor[0]
 
   const Strong: FCC = ({ children }) => (
     <StrongModifier color={textColor}>{children}</StrongModifier>
   )
 
-  const [errors, setErrors] = useState<string[]>([])
-  const [loading, setLoading] = useState(false)
-  const [success, setSuccess] = useState(false)
-  const [formValues, setFormValues] = useState<AskFormProps>(initialFormValues)
-  const characterLimit = 1000
-  const overLimit = formValues.message.length > characterLimit
-
-  const handleSubmit = (event: React.FormEvent<AskFormElements>) => {
-    setSuccess(false)
-    event.preventDefault()
-    const name = formValues.name
-    const message = formValues.message
-    const antirobot = event.currentTarget.elements.antirobot.value
-
-    if (antirobot) {
-      return
-    }
-
-    if (!message) {
-      setErrors(['Oops, o nome é opcional, mas a mensagem não!'])
-    }
-
-    if (message.length > characterLimit) {
-      setErrors([`Ooops, há um limite de ${characterLimit} caracteres`])
-    }
-
-    setLoading(true)
-    postToNotion({ name, message })
-      .then(result => {
-        if (result.errors && result.errors?.length > 0) {
-          const errorMessages = result.errors.map(error => error.msg)
-          setErrors(errorMessages)
-        }
-
-        setSuccess(true)
-        setFormValues(initialFormValues)
-      })
-      .catch(error => {
-        setErrors(['An unknown error happenned, please contact Angelo'])
-        console.error(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
-  const removeError = () => {
-    setErrors([])
-  }
-
-  const handleChange = (prop: keyof AskFormProps, value: string) => {
-    setFormValues(state => ({
-      ...state,
-      [prop]: value,
-    }))
-  }
+  const successMessage = (
+    <>
+      <p>
+        <Strong>Enviado!</Strong>
+      </p>
+      <p>
+        O que acha de fazer outra pergunta? Não esqueça de assinar o podcast{' '}
+        <Link href="/podcast">Cronofobia</Link> para escutar sua resposta!
+      </p>
+    </>
+  )
 
   return (
-    <form onSubmit={handleSubmit} className="w-full mb-8">
+    <div className="w-full mb-8">
       <h2 className="mb-4 h2-as-h1">
         Ask me <span className={textColor}>anything</span>
       </h2>
@@ -127,88 +70,32 @@ export const AskForm = () => {
         </Strong>
         .
       </p>
-      {/* Avoid spam by adding a hidden field that can only be reached by machines */}
-      <input
-        type="text"
-        defaultValue=""
-        name="antirobot"
-        tabIndex={-1}
-        className="absolute left-[-5000px]"
-        aria-hidden="true"
-      />
-      <div className="flex flex-col gap-4">
-        <div>
-          <label htmlFor="message" className="flex-1 block w-full">
-            Nome (opcional):
-          </label>
-          <input
-            type="text"
-            name="name"
-            value={formValues.name}
-            onChange={e => handleChange('name', e.target.value)}
-            className="w-full"
-            placeholder="Mr Spock"
-            onClick={removeError}
-          />
-        </div>
-        <div>
-          <label htmlFor="message" className="flex-1 block w-full">
-            Pergunta:
-          </label>
-          <textarea
-            value={formValues.message}
-            name="message"
-            className="w-full"
-            onChange={e => handleChange('message', e.target.value)}
-            placeholder="Angelo, fale sobre..."
-            onClick={removeError}
-            required
-          />
-          <small className={`${overLimit ? 'text-red' : ''}`}>
-            {formValues.message.length}/{characterLimit}
-          </small>
-        </div>
-        {loading ? (
-          <div className="flex flex-col items-center justify-center w-full">
-            <Loader />
-          </div>
-        ) : (
-          <input
-            type="submit"
-            value="Send"
-            name="send"
-            className={`${bgColor} ${borderColor} mt-2`}
-            disabled={overLimit}
-          />
-        )}
-      </div>
-      {errors.length > 0 && (
-        <div className="mt-2 text-sm">
-          <p className="mt-2 mb-0 text-sm font-bold text-red">
-            Bzz Bzz! Error! Error!
-          </p>
-          <ul>
-            {errors.map((error, index) => {
-              return (
-                <li key={index} className="list-disc list-inside">
-                  {error}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      )}
-      {success && (
-        <div className="mt-4">
-          <p>
-            <Strong>Enviado!</Strong>
-          </p>
-          <p>
-            O que acha de fazer outra pergunta? Não esqueça de assinar o podcast{' '}
-            <Link href="/podcast">Cronofobia</Link> para escutar sua resposta!
-          </p>
-        </div>
-      )}
-    </form>
+
+      <Form<FormProps>
+        fetcher={postToNotion}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        submit={{
+          successMessage: successMessage,
+          label: 'Enviar',
+        }}
+      >
+        <Input
+          label="Nome (opcional)"
+          name="name"
+          placeholder="Mr Spock"
+          type="text"
+        />
+
+        <Input
+          label="Pergunta:"
+          name="message"
+          placeholder="Angelo, fale sobre..."
+          type="textarea"
+          as="textarea"
+          textAreaLimit={1000}
+        />
+      </Form>
+    </div>
   )
 }
