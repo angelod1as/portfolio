9d3d369ab851b69e0b27bd1fe8bcb3a42370996d
Angelo Dias <oiangelodias@gmail.com>
2023-01-03 15:17:07 +0100

    Add party redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/festa/:slug*',
+        destination: '/party/:slug*', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
 }
