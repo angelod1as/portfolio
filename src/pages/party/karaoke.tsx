7b0d974c5ae44b18e8ead16a40dce2b4db6ec095
Angelo Dias <oiangelodias@gmail.com>
2023-01-04 12:00:57 +0100

    Add social image

--- src/pages/party/karaoke.tsx
+++ src/pages/party/karaoke.tsx
@@ -2,9 +2,30 @@ import { GetStaticProps, NextPage } from 'next'
 import React from 'react'
 import { RandomColors, randomColors } from 'src/helpers/colors'
 import { KaraokeParty } from '#components/pages/Party/karaoke'
+import Head from 'next/head'
 
 const PartyPage: NextPage = () => {
-  return <KaraokeParty />
+  const socialImagePath = 'angelodias.com.br/party/social/karaoke.png'
+
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          key="og:image"
+          content={'https://' + socialImagePath}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@oicronofobico" />
+        <meta
+          property="twitter:image"
+          key="twitter:image"
+          content={'https://' + socialImagePath}
+        />
+      </Head>
+      <KaraokeParty />
+    </>
+  )
 }
 
 export default PartyPage
