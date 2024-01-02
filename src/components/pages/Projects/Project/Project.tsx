85b5b18059cb9cfe34bc9a20d0d0bd2502e8beba
Angelo Dias <oiangelodias@gmail.com>
2024-01-02 16:41:52 -0300

    Add view all projects CTA

--- src/components/pages/Projects/Project/Project.tsx
+++ src/components/pages/Projects/Project/Project.tsx
@@ -1,3 +1,4 @@
+import { CTA } from '#components/common/CTA'
 import { MDX } from '#components/common/MDX'
 import { NewHead } from '#components/common/NewHead'
 import { BlogPostProps } from '#pages/blog/[slug]'
@@ -21,6 +22,7 @@ export const Project: FCC<BlogPostProps> = ({ content }) => {
         />
       )}
       <MDX mdx={{ compiledSource }} metadata={metadata} type="projects" />
+      <CTA inner href="/projects" content="View all projects" />
     </>
   )
 }
