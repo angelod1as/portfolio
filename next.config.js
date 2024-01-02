cb15cb4d38b077ebf9008730458f3aa805dccbc7
Angelo Dias <oiangelodias@gmail.com>
2024-01-02 15:28:05 -0300

    Remove redirects

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/festa/:slug*',
-        destination: '/party/:slug*', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-      {
-        source: '/pergunte',
-        destination: '/cronofobia',
-        permanent: true,
-      },
-    ]
-  },
 }
