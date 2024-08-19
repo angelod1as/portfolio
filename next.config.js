8d95aa5f1c3e2b7e65285dc24b4c873f102849a5
Angelo Dias <oiangelodias@gmail.com>
2024-08-19 15:09:42 -0300

    add calendar link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+    return [
+      {
+        source: '/call',
+        destination: 'https://calendar.app.google/fXMaK5im14WKQZVs8',
+        permanent: true,
+      },
+    ]
+  },
 }
