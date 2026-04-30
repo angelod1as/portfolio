627ba2842b97e3707d80b022e521e8ff1f84ce04
Angelo Dias <oiangelodias@gmail.com>
2026-04-30 10:01:11 -0300

    add collections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("blog");
+
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
