a014e5bc38bec3f162923d79619a354badf2254c
Angelo Dias <oiangelodias@gmail.com>
2026-04-30 19:17:56 -0300

    Update 1

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,12 +8,6 @@ const blog = defineCollection({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
   }),
 });
 
