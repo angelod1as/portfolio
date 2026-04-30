ff8faf434664e6282af06ee76b08d9f4045ebfc8
Angelo Dias <oiangelodias@gmail.com>
2026-04-30 09:44:40 -0300

    Finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [preact()],
+  site: "https://www.angelodias.com.br",
 });
