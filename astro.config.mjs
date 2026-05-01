001786a88ab9fddeb90067792f27cb36a0d085b4
Angelo Dias <oiangelodias@gmail.com>
2026-05-01 12:24:08 -0300

    remove preact (will add if necessary)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [],
   site: "https://www.angelodias.com.br",
 });
