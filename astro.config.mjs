e7901eb2fc6c22c5ac51e543bfb284a58093d089
Angelo Dias <oiangelodias@gmail.com>
2026-05-04 18:50:59 -0300

    updates fonts and add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [mdx()],
   site: "https://www.angelodias.com.br",
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "DM Serif Display",
+      cssVariable: "--font-display",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Lora",
+      cssVariable: "--font-text",
+    },
+  ],
 });
