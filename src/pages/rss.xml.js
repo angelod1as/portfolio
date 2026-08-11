c3c14b931e1e7fc988b6453137c855552e471a75
Angelo Dias <oiangelodias@gmail.com>
2026-08-11 19:50:20 -0300

    add kind to rss categories

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -15,7 +15,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      categories: post.data.categories,
+      categories: [...post.data.categories, ...post.data.kind],
       link: `/blog/${post.id}/`,
     })),
     customData: `<language>en-us</language>`,
